516219d22124ce2a6c7bd670af580de592ac52c0
RahulG <rahulgd77@yahoo.co.in>
2017-01-04 12:25:25 +0000

    Confluence install playbooks and rollback jenkins deployment

---
+++ jenkins-ansible-rollback.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export host=$1
+export ANSIBLE_HOST_KEY_CHECKING=False
+export workspace_dir=/opt/workspace/devops-tooling
+ansible-playbook ${workspace_dir}/rollback.yml -e host=$host -i ${workspace_dir}/hosts 
